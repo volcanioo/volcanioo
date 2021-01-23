c9b9fc5188cee42777267d909c37b18fd0a13e35
volcanioo <arayuzgelistirici@gmail.com>
2021-01-23 15:14:11 +0100

    🔧 (Tailwind): new color palette created

--- virtual-desk/tailwind-css/tailwind.config.js
+++ virtual-desk/tailwind-css/tailwind.config.js
@@ -1,29 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
+    backgroundColor: theme => ({
+      primary: {
+        100: '#3490dc',
+        500: '#ffed4a',
+        600: '#e3342f',
       },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+    }),
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
